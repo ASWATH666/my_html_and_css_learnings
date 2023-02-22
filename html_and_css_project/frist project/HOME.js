2021ca8eaf2c1d3a00c05874d7662002487708d1
ASWATH666 <aswathaswath4321@gmail.com>
2023-02-22 19:50:51 +0530

    this is new'

---
+++ html_and_css_project/frist project/HOME.js
@@ -0,0 +1,21 @@
+var t=10;
+var s=110.5;
+console.log(t);
+console.log(s);
+alert("going good ");
+document.write('<h1>this the good tiime to learn the javascript');
+var a=prompt("are u a robot");
+document.getElementById("caal").innerHTML=a;
+switch(a){
+    case "yes":document.write("<h1>dont hurt humans!!!<\h1>");
+    break;
+
+    case "no":document.write("<h1>dont hurt robots!!!</h1>");
+    break;
+}
+function changecolours(a){
+    document.getElementById("caal").style.color="lightblue";
+}
+function changered(a){
+    document.getElementById("caal").style.color="brown";
+}
